857f372bb76c89c4b94bbfdeb296102f188671e5
serdar yildiz <serdaryildiydev@gmail.com>
2023-02-11 01:42:22 +0300

    simple types learned

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let name: String = "Serdar"
+let age = 21
+
+
+var year: Int = 23
+    
+year = 2023
+
+//type inference
+//type safety
+
+var x: Int
+x = 12
+print("x value  ",x)
+
+let isim = "serdar"
+
+print("name lenght ",name.count)
+
+var dynamicLink = "www.asdsa/"
+dynamicLink.append("1232")
+
+//string interpolation
+let count = 21
+print("my age  is \(count)")
